87408039820d15dcbdc44202a453866a43e83519
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-25 16:07:43 -0500

    Adding table info of albums selling over 30 mil

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', '1982', '45.4', 'Pop'),
+	('AC/DC', 'Back in Black', '1980', '25.9', 'Hard Rock'),
+	('Pink Floyd', 'The Dark Side of the Moon', '1973', '22.7', 'Progressive Rock'),
+	('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'R&B'),
+	('Meat Loaf', 'Bat Out of Hell', '1977', '20.6', 'Hard Rock'),
+	('Eagles', 'Their Greatest Hits (1971–1975)', '1976', '32.2', 'Soft Rock'),
+	('Bee Gees', 'Saturday Night Fever', '1977', '19', 'Disco'),
+	('Fleetwood Mac', 'Rumors', '1977', '27.9', 'Soft Rock');
