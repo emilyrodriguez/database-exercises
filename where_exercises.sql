32e4707118f58c4fbfc7f89fdfdce5b795bd0987
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-07-27 11:56:34 -0500

    Changed code to chain WHERE statments for more complex queries

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,10 +1,11 @@
--- Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
-SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya','Maya');
--- Employees whose last name starts with 'E' — 7,330 rows.
-SELECT last_name FROM employees WHERE last_name LIKE 'e%';
--- Employees hired in the 90s — 135,214 rows.
-SELECT * FROM employees WHERE hire_date LIKE '199%';
--- Employees born on Christmas — 842 rows.
-SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
--- Employees with a 'q' in their last name — 1,873 rows.
-SELECT last_name FROM employees WHERE last_name LIKE '%q%';
+-- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN. Add a condition to find everybody with those names who is also male — 441 rows.
+SELECT first_name FROM employees WHERE (first_name='Irena' OR first_name='Vidya' OR first_name='Maya') AND gender = 'M';
+
+-- find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT last_name FROM employees WHERE last_name LIKE 'e%e';
+
+-- Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT last_name FROM employees WHERE (last_name LIKE '%q%') AND (last_name NOT LIKE '%qu%');
